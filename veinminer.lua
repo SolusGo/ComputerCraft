e2210e00376671ba0a1d0d757995289fd590c165
SolusGo <gabrielcarylat1@gmail.com>
2021-04-13 21:28:09 +1000

    Testing number one

--- veinminer.lua
+++ veinminer.lua
@@ -1,1 +1,25 @@
 print("I am just testing something v2")
+
+local function staircase()
+
+    return false
+end 
+
+
+while true do
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
+    for i = 1, 5 do
+        turtle.digDown()
+        turtle.dig()
+        turtle.forward()
+
+    end 
+    
+    
+
+
+
+
+end
