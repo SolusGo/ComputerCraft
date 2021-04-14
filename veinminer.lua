bffeea1adc9b6b310e7f40c7e35f656033d7976c
SolusGo <gabrielcarylat1@gmail.com>
2021-04-14 10:39:32 +1000

    added more features

--- veinminer.lua
+++ veinminer.lua
@@ -7,19 +7,53 @@ end
 
 local ylevel = 0
 
+local function checkFuel()
+    if turtle.getFuelLevel() == "unlimited" then
+        turtle.refuel()
+    end
+
+    if turtle.getItemCount(1) < 64 then
+        for i = 1, 16 do
+            data = turtle.getItemDetail(i)
+            firstslot = turtle.getItemDetail(1)
+            if data.name == "minecraft:coal" and firstslot.name == "minecraft:coal" then
+                turtle.select(i)
+                turtle.transferTo(1)
+            end
+        end
+    end
+
+end
+
+
+local function succ()
+    if not turtle.suck() then
+        os.exit()
+    end
+end
 
 while true do
     turtle.dig()
+    succ()
     turtle.forward()
+    turtle.digUp()
+    succ()
     turtle.turnRight()
     for i = 1, 5 do
         turtle.digDown()
+        succ()
         turtle.dig()
+        succ()
         if not turtle.forward() then
             turtle.refuel()
         end
+        turtle.digUp()
+        succ()
     end
     turtle.dig()
+
+    succ()
+
     turtle.turnRight()
     turtle.turnRight()
     for i = 1, 5 do
